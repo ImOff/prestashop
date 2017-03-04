0db1b1891f50d788bdb5064dec6d077a30f04a4c
WTFlay <schrie_f@etna-alternance.net>
2017-03-04 23:41:43 +0100

    Finish getQuery of Select and add all profile query.

--- statssegmentation/classes/Select.php
+++ statssegmentation/classes/Select.php
@@ -10,7 +10,7 @@ class Select extends Type
 
 	private $labelForAll = null;
 
-	function __construct($query, $tableName, $labelForAll)
+	function __construct($query, $tableName, $labelForAll, $parameters = ['1', '5'])
 	{
 		parent::__construct($query);
 		$this->tableName = $tableName;
@@ -32,13 +32,13 @@ class Select extends Type
 		$html = null;
 
 		$html .= '<td><div class="multiselect"><div class="selectBox" onclick="showCheckboxes(\'' . $this->tableName . '\')">';
-		$html .= '<select name="s_' . $name . '"><option value="0">' . $this->labelForAll . '</option></select>';
+		$html .= '<select name="s_' . $this->name . '"><option value="0">' . $this->labelForAll . '</option></select>';
 		$html .= '<div class="overSelect"></div></div>';
 		$html .= '<div id="' . $this->tableName . '" class="checkboxes">';
 		$i = 0;
 		foreach ($this->list as $element) {
 			$html .= '<label for="' . $i . "_" . $this->tableName . '">';
-			$html .= '<input type="checkbox" id="' . $i . "_" . $this->tableName . '"/>';
+			$html .= '<input type="checkbox" name="' . $i . "_" . $this->tableName . '"/>';
 			$html .= '<span>' . $element['name'] . '</span></label>';
 			$i++;
 		}
@@ -48,6 +48,26 @@ class Select extends Type
 
 	function getQuery($name, $operator = true)
 	{
+		$values = [];
+
+		if ($operator)
+			$query = str_replace("#", "IN", $this->query);
+		else
+			$query = str_replace("#", "NOT IN", $this->query);
+
+		for ($i = 0; $i < count($this->list); $i++)
+		{
+			if (Tools::getValue($i . "_" . $this->tableName))
+				$values[] = $i + 1;
+		}
+
+		if (!count($values))
+		{
+			for ($i = 0; $i < count($this->list); $i++)
+				$values[] = $i + 1;
+		}
+		$query = str_replace("@", implode(",", $values), $query);
+
 		return ($query);
 	}
 }
